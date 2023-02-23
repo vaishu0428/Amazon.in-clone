d3bf9405d5c298c68004566b758701024830a932
Satan Sharma <satansharma7549@gmail.com>
2023-02-23 21:59:35 +0530

    Cloudinary install and setup

--- backend/index.js
+++ backend/index.js
@@ -1,35 +1,43 @@
 /* importing necessary modules */
 const express = require("express");
-const dotenv = require("dotenv")
-dotenv.config()
+const dotenv = require("dotenv");
+dotenv.config();
 const cors = require("cors");
 const ConnectDb = require("./config/db.connect");
 const productRouter = require("./routes/products.route");
 const { userRoutes } = require("./routes/user.route");
+const cartRouter = require("./routes/cart.route");
 // const { authenticateToken } = require("./middleware/userAuth.middleware");
 
-const app = express()
-app.use(express.json())
-app.use(cors({
-  origin:"*"
-}))
+const fileupload = require("express-fileupload");
 
-app.get("/", (req,res)=>{
-  res.send("<h1>Welcome to backend home route</h1>")
-}) 
+const app = express();
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.use(
+  fileupload({
+    useTempFiles:true
+  })
+);
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to backend home route</h1>");
+});
 
 /* import all routes */
 
-app.use("/product",productRouter)
-app.use("/user",userRoutes)
+app.use("/product", productRouter);
+app.use("/user", userRoutes);
+app.use("/cart", cartRouter);
 // app.use(authenticateToken)
-
-
-
 
 const port = process.env.PORT || 8090;
 
 app.listen(port, async () => {
-await ConnectDb()
+  await ConnectDb();
   console.log(`Server running on http://localhost:${port}`);
-})  
+});
