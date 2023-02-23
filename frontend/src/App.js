dd2e84b891b962f334fb1a3d8d55f2954b11413b
Prashant7970 <prashant887359@gmail.com>
2023-02-23 21:22:12 +0530

    adding cartpage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,16 @@
 import './App.css';
 import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
+import Cart from './Pages/cart/Cart';
+import Home from './Pages/Home';
 
 function App() {
   return (
 
     <div className="App">
+      <Navbar/>
+     <Cart/>
+      <Footer/>
 
     </div>
   );
