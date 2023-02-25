fbb068ed5151c4f2af2f78aae379cbc3ae3e28ee
vaishu0428 <vaishnvischaudhari@gmail.com>
2023-02-25 22:17:59 +0530

    Childs responsive

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ import Childs from "./Pages/Child"
 function App() {
   return (
     <div>
-  
+
     </div>
   );
 }
--- frontend/src/components/Men/ChildData.jsx
+++ frontend/src/components/Men/ChildData.jsx
@@ -30,20 +30,21 @@ const ChildData = () => {
     <Box>
       <ChildSlider/>
       <Box p='2' m="2" borderBottom="1px solid gray">
-      <Heading as='h1' size='lg' color={'gray'}>Childs Shopee ({Child_product.length} items)</Heading>
+      <Heading as='h1' size={['sm','lg','lg']} color={'gray'}>Childs Shopee ({Child_product.length} items)</Heading>
       </Box>
       {Child_product.length==0? <EmptyCart/> :
-     <Grid templateColumns={'repeat(4,1fr)'}
+     <Grid templateColumns={['repeat(1,1fr)','repeat(3,1fr)','repeat(4,1fr)']}
      gap='3'>
     { Child_product.map((ele)=>(
       
         <GridItem key={ele.id} boxShadow="rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px">
           <Image src={ele.image}/>
           <Box p='2'>
+          <Heading as='h4' size='md' textAlign="center">--{ele.brand}--</Heading>
           <Text color={"gray"} as="b">{ele.title}</Text>
           <Text color={"green"}>MRP : ₹{ele.price}</Text>
           <Text color={"gray"}>Type :{ele.type}</Text>
-          <Text color={"gray"}>Brand:{ele.brand}</Text>
+          {/* <Text color={"gray"}>Brand:{ele.brand}</Text> */}
           <Text color={"gray"}>Qty:{ele.quantity}</Text>
           </Box>
         </GridItem>
