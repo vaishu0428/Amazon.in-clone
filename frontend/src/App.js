2199f7aec39008fe227388d9fe5781f9288af4de
AsifShaikh01 <asifshaikh0024@gmail.com>
2023-02-26 01:17:40 +0530

    admin order page added by asif

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,7 @@
 
 
-import AdminUserCard from './admin/adminComponents/usercard/AdminUserCard';
-import AdminProducts from './admin/adminPages/AdminProducts';
-import AdminUpdateProductPage from './admin/adminPages/AdminUpdateProductPage';
-import AdminUsersPage from './admin/adminPages/AdminUsersPage';
-import Home from './admin/adminPages/Home';
-import AllRoutes from './admin/adminRoutes/AllRoutes';
+
+
 import './App.css';
 
 
@@ -20,6 +16,7 @@ function App() {
 
 <div className="App">
 <Pageroutes/>
+ 
        </div>
   );
 }
